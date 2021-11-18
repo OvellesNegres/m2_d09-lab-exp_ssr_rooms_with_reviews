fc4130c82e22690650d831d95de1614b5bac1064
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-18 10:31:53 +0100

    done sister route for signup

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/signup')
+.get((req, res) => {
+  res.render('signup-form');
+})
+.post((req, res)=>{
+const {username, email, password} = req.body
+res.send(req.body)
+})
 
 module.exports = router;
