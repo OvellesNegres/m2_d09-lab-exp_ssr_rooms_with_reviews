d772c38199c206d4fb9ac9c5c8bb8ad3767a65f7
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-18 14:35:40 +0100

    done basic room read and create

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index.route');
+var usersRouter = require('./routes/users.route');
+var roomsRouter = require('./routes/room.route');
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
