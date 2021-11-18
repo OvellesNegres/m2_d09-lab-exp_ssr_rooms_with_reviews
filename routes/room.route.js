29b438a4859bda33f672acd42db7403d4efbbb32
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-18 21:12:05 +0100

    done room routes

--- routes/room.route.js
+++ routes/room.route.js
@@ -4,36 +4,30 @@ var router = express.Router();
 const User = require("../models/User.model")
 const Room = require("../models/Room.model")
 
-/* GET home page. */
 router.get('/', async (req, res) => {
-    let listRooms = []
-    let error = null
     try {
         listRooms = await Room.find()
+        res.render('rooms/rooms-list', { rooms: listRooms});
     }
-    catch (e) {
-        error = { errType: "DB_ERR", msg: e }
-    } finally {
-        res.render('rooms/rooms-list', { rooms: listRooms, error });
+    catch (error) {
+        res.render('rooms/rooms-list', {error});
     }
 });
 
 router.route("/new")
     .get(async (req, res) => {
         const users = await User.find()
-        res.render("rooms/new-room", {users})
+        res.render("rooms/new-room", { users })
     })
     .post(async (req, res) => {
         const { name, description, owner } = req.body
-        if (!name || !description || !owner) {
-            res.render("rooms/new-room", { error: { type: "CRROOM_ERR", msg: "All fileds required" } })
-        }
+        console.log("req.body: ", req.body)
         try {
+            if (!name || !description || !owner) throw new Error("All fileds required")  // res.render("rooms/new-room", { error: { type: "CRROOM_ERR", msg: "All fileds required" } })
             const newRoom = await Room.create({ name, description, owner })
-        } catch (e) {
-            res.render("rooms/new-room", { error: { type: "DB_ERR", msg: "WTF the DB broke" } })
-        } finally {
             res.redirect("/rooms")
+        } catch (e) {
+            res.render("rooms/new-room", { error })
         }
     })
 
--- routes/users.route.js
+++ routes/users.route.js
@@ -4,51 +4,54 @@ const bcrypt = require("bcrypt")
 const User = require("../models/User.model")
 
 router.route('/signup')
-.get((req, res) => {
-  res.render('signup-form');
-})
-.post( async (req, res)=>{
-const {username, email, password} = req.body
-if(!username || !email || !password){
-  res.render("signup-form", { username, email, error:{type: "CRED_ERR", msg: "Missing credentials"}})
-}
+  .get((req, res) => {
+    res.render('signup-form');
+  })
+  .post(async (req, res) => {
+    const { username, email, password } = req.body
+    try {
+      if ( !username || !password || !email || !email.includes('@')) throw Error({ type: "CRED_ERR", msg: "Missing credentials" })
 
-const user = await User.findOne({email})
-if(user){
-  res.render("signup-form", { username, email, error:{type: "USR_ERR", msg: "Email exists"}})
-}
+      const user = await User.findOne({ email })
+      if (user) throw Error({ type: "USR_ERR", msg: "Email exists" })
 
-const salt = bcrypt.genSaltSync(5)
-const hashPwd = bcrypt.hashSync(password, salt)
+      const salt = bcrypt.genSaltSync(5)
+      const hashPwd = bcrypt.hashSync(password, salt)
 
-const newUser = await User.create({username, email, password: hashPwd})
-res.send(newUser)
-})
+      const newUser = await User.create({ username, email, password: hashPwd })
+      res.send(newUser)
+    }
+    catch (error) {
+      res.render("signup-form", { username, email, error })
+    }
+
+  })
 
 router.route("/login")
-.get((req, res) =>{ res.render("login-form")})
-.post(async (req, res)=>{
-  const {email, password} = req.body
-  if(!email || !password){res.render("login-form", {error:{type: "CRED_ERR", msg: "Missing credentials"}}) }
+  .get((req, res) => { res.render("login-form") })
+  .post(async (req, res) => {
+    const { email, password } = req.body
+    try {
+      if (!email || !password) throw Error({ type: "CRED_ERR", msg: "Missing credentials" })
 
-  const loggedinUser = await User.findOne({email})
-  if(!loggedinUser) {res.render("login-form", {error:{type: "USER_ERR", msg: "User does not exist"}}) }
+      const loggedinUser = await User.findOne({ email })
+      if (!loggedinUser) throw Error({ type: "USER_ERR", msg: "User does not exist" })
 
-  const pswIsCorrect = bcrypt.compareSync(password, loggedinUser.password)
+      const pswIsCorrect = bcrypt.compareSync(password, loggedinUser.password)
+      if (!pswIsCorrect) throw Error({ type: "PWD_ERR", msg: "Password incorrect" })
 
-if(pswIsCorrect){
-  req.session.loggedinUser = loggedinUser
-  res.redirect("/")
-}else{
-  res.render("login-form", {error:{type: "PWD_ERR", msg: "Password incorrect"}})
-}
-})
+      req.session.loggedinUser = loggedinUser
+      res.redirect("/")
+    } catch (error) {
+      res.render("login-form", { email, error })
+    }
+  })
 
 router.get('/logout', (req, res) => {
-	req.session.destroy((err) => {
-		if (err) res.redirect('/');
-		else res.redirect('/users/login');
-	});
+  req.session.destroy((err) => {
+    if (err) res.redirect('/');
+    else res.redirect('/users/login');
+  });
 });
 
 
