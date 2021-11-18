e72de936fa8883bee05744bec3f051bbf606ce51
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-18 11:43:04 +0100

    done db connection

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require("bcrypt")
+const User = require("../models/User.model")
+
 
 /* GET users listing. */
 router.route('/signup')
 .get((req, res) => {
   res.render('signup-form');
 })
-.post((req, res)=>{
+.post( async (req, res)=>{
 const {username, email, password} = req.body
-res.send(req.body)
+if(!username || !email || !password){
+  res.render("signup-form", { username, email, error:{type: "CRED_ERR", msg: "Missing credentials"}})
+}
+
+const user = await User.findOne({email})
+if(user){
+  res.render("signup-form", { username, email, error:{type: "USR_ERR", msg: "Email exists"}})
+}
+
+const salt = bcrypt.genSaltSync(5)
+const hashPwd = bcrypt.hashSync(password, salt)
+
+const newUser = await User.create({username, email, password: hashPwd})
+res.send(newUser)
+
+
+
 })
 
 module.exports = router;
